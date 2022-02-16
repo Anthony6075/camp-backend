083c61340b2d3732da14a71271654656bfd7f292
Anthony Li <Anthony6075@163.com>
2022-02-16 11:58:58 +0800

    删除model.go中的struct的json tag

--- types/model.go
+++ types/model.go
@@ -4,19 +4,19 @@ package types
 // 账号名：JudgeAdmin 密码：JudgePassword2022
 
 type TMember struct {
-	UserID       string    `json:"userID" gorm:"primaryKey"`
-	Nickname     string    `json:"nickname"`
-	Username     string    `json:"username" gorm:"unique;not null"`
+	UserID       string `gorm:"primaryKey"`
+	Nickname     string
+	Username     string    `gorm:"unique;not null"`
 	Password     string    `json:"-" gorm:"not null"`
-	UserType     UserType  `json:"userType" gorm:"not null"`
+	UserType     UserType  `gorm:"not null"`
 	IsDeleted    bool      `json:"-" gorm:"not null;default:false"`
 	LearnCourses []TCourse `gorm:"many2many:learn_courses;joinForeignKey:user_id;joinReferences:course_id;"`
 }
 
 type TCourse struct {
-	CourseID  string  `json:"courseID" gorm:"primaryKey"`
-	Name      string  `json:"name" gorm:"not null;unique"`
-	TeacherID string  `json:"teacherID" gorm:"size:191"`
+	CourseID  string  `gorm:"primaryKey"`
+	Name      string  `gorm:"not null;unique"`
+	TeacherID string  `gorm:"size:191"`
 	Teacher   TMember `json:"-" gorm:"foreignKey:TeacherID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Capacity  int     `json:"-" gorm:"not null;default:0"`
 }
